b16d1dc3f0e82d201166f95562d6196be0a9b043
talal-web <maliktalal910@gmail.com>
2025-11-28 15:55:33 -0800

    chore: add admin register router in app.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 
 import authRoutes from "./routes/authRoutes.js";
+import adminRoutes from "./routes/adminRoutes.js"
 import studentRoutes from "./routes/studentRoutes.js";
 import eventRoutes from "./routes/eventRoutes.js";
 import diaryRoutes from "./routes/diaryRoutes.js";
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
 app.use("/api/auth", authRoutes);
+app.use("/api/admin", adminRoutes);
 app.use("/api/students", studentRoutes);
 app.use("/api/events", eventRoutes);
 app.use("/api/diary", diaryRoutes);
