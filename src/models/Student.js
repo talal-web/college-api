400325b0aa2affb4ae615a7024b8d2d9f131972e
talal-web <maliktalal910@gmail.com>
2025-11-28 21:00:55 -0800

    feat: add userID and change required property for some fields

--- src/models/Student.js
+++ src/models/Student.js
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
 
 const studentSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  rollNo: { type: String, required: true, unique: true },
-  department: { type: String, required: true },
-  semester: { type: Number, required: true },
+  userID: { type: String, required: true, unique: true },
+  name: { type: String},
+  rollNo: { type: String},
+  department: { type: String},
+  semester: { type: Number},
   image: { type: String },
-  password: { type: String, required: true }
+  password: { type: String, required: true}
 }, { timestamps: true });
 
 export default mongoose.model("Student", studentSchema);
