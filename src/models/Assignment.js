3683722fe1940a75ff58230dfd45cffb4015c9f6
talal-web <maliktalal910@gmail.com>
2025-11-30 10:13:31 -0800

    improve: schema of assignment by adding more fields

--- src/models/Assignment.js
+++ src/models/Assignment.js
@@ -1,10 +1,17 @@
 import mongoose from "mongoose";
 
-const assignmentSchema = new mongoose.Schema({
-  subject: String,
-  description: String,
-  file: String,
-  dueDate: Date
-}, { timestamps: true });
+const assignmentSchema = new mongoose.Schema(
+  {
+    subject: { type: String, required: true },
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    file: { type: String},
+    department: { type: String, required: true },
+    semester: { type: Number, required: true },
+    dueDate: { type: Date, required: true },
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "Admin", required: true }
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model("Assignment", assignmentSchema);
