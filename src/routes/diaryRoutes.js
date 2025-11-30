cff571a4b992e1eb90be6bc15a2301723494025f
talal-web <maliktalal910@gmail.com>
2025-11-29 21:16:27 -0800

    feat: add admin only for diary creation

--- src/routes/diaryRoutes.js
+++ src/routes/diaryRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { protect } from "../middlewares/authMiddleware.js";
 import { addDiary, getDiary } from "../controllers/diaryController.js";
+import {adminOnly} from "../middlewares/adminMiddleware.js"
 
 const router = express.Router();
 
-router.post("/add", protect, addDiary);
+router.post("/add", protect, adminOnly, addDiary);
 router.get("/", protect, getDiary);
 
 export default router;
