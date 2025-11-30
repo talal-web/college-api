5eb44ec50d39291ff4dc8b237a0a25a9050cdd13
talal-web <maliktalal910@gmail.com>
2025-11-30 12:22:55 -0800

    feat: add role field for students

--- src/models/Student.js
+++ src/models/Student.js
@@ -2,12 +2,13 @@ import mongoose from "mongoose";
 
 const studentSchema = new mongoose.Schema({
   userID: { type: String, required: true, unique: true },
+  password: { type: String, required: true},
   name: { type: String},
   rollNo: { type: String},
   department: { type: String},
   semester: { type: Number},
+  role: { type: String, default: "student" },
   image: { type: String },
-  password: { type: String, required: true},
   profileComplete: { type: Boolean, default: false}
 }, { timestamps: true });
 
