fa040712e89fbbec8ea13c4b826cd790803ea691
talal-web <maliktalal910@gmail.com>
2025-11-29 21:09:58 -0800

    feat: include more fields in Diary schema

--- src/models/Diary.js
+++ src/models/Diary.js
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
 
 const diarySchema = new mongoose.Schema({
-  studentId: { type: mongoose.Schema.Types.ObjectId, ref: "Student" },
-  content: String,
-  date: Date
+  subject: { type: String, required: true },
+  topic: { type: String, required: true },
+  outline: [{ type: String, required: true }], // array of main points/headlines
+  department: { type: String, required: true },
+  semester: { type: Number, required: true },
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "Admin", required: true }
 }, { timestamps: true });
 
 export default mongoose.model("Diary", diarySchema);
