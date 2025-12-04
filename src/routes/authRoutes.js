048a8143f28f88f26c93be5ca61d1ac1749c2468
talal-web <maliktalal910@gmail.com>
2025-12-03 22:41:28 -0800

    feat: add /me route for sending user data in response

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { login } from "../controllers/authController.js";
+import { getMe, login } from "../controllers/authController.js";
 import { completeProfile } from "../controllers/profileCompletionController.js";
+import { protect } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/login", login);
 router.post("/profile/complete", completeProfile);
+router.get("/me", protect, getMe);
+
 
 export default router;
