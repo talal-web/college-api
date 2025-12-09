c05a8eedc59e75209c7218b8d37205d2e84cd455
talal-web <maliktalal910@gmail.com>
2025-12-09 21:38:46 +0500

    update expire token time to 1 hour

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -2,6 +2,6 @@ import jwt from "jsonwebtoken";
 
 export const generateToken = (id, role) => {
   return jwt.sign({ id, role }, process.env.JWT_SECRET, {
-    expiresIn: "30d",
+    expiresIn: "1h",
   });
 };
