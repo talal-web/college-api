7229b37bb57b02aaad23f06ed6c156912275ce8a
talal-web <maliktalal910@gmail.com>
2025-12-09 21:41:20 +0500

    add file filter in multer upload

--- src/utils/upload.js
+++ src/utils/upload.js
@@ -2,12 +2,18 @@ import multer from "multer";
 import path from "path";
 
 const storage = multer.diskStorage({
-  destination: "uploads/",
+  destination: (req, file, cb) => cb(null, "uploads/"),
   filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
-  }
+    const ext = path.extname(file.originalname);
+    cb(null, Date.now() + ext);
+  },
 });
 
-const upload = multer({ storage });
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image/")) cb(null, true);
+  else cb(new Error("Only images are allowed"), false);
+};
+
+const upload = multer({ storage, fileFilter });
 
 export default upload;
