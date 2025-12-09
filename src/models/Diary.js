365ae2ae5ba731d6c43c59acd5ed1e001cd0e832
talal-web <maliktalal910@gmail.com>
2025-12-09 21:26:26 +0500

    fix array for outline field

--- src/models/Diary.js
+++ src/models/Diary.js
@@ -3,8 +3,7 @@ import mongoose from "mongoose";
 const diarySchema = new mongoose.Schema({
   subject: { type: String, required: true },
   topic: { type: String, required: true },
-  outline: [{ type: String, required: true }], // array of main points/headlines
-  date: { type: Date, default: Date.now},
+  outline: { type: [String], required: true }, // array of main points/headlines
   department: { type: String, required: true },
   semester: { type: Number, required: true },
   createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "Admin", required: true }
