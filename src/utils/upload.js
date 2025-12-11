97c92fc6bf12f159111b3131a1b7f5513701baa8
talal-web <maliktalal910@gmail.com>
2025-12-11 11:34:50 +0500

    set cloudinary path for uplaoding images

--- src/utils/upload.js
+++ src/utils/upload.js
@@ -1,19 +1,14 @@
+// /utils/upload.js
 import multer from "multer";
-import path from "path";
+import storage from "../config/cloudinary";
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, "uploads/"),
-  filename: (req, file, cb) => {
-    const ext = path.extname(file.originalname);
-    cb(null, Date.now() + ext);
-  },
-});
-
+// Filter to allow only image files
 const fileFilter = (req, file, cb) => {
   if (file.mimetype.startsWith("image/")) cb(null, true);
   else cb(new Error("Only images are allowed"), false);
 };
 
+// Multer upload instance
 const upload = multer({ storage, fileFilter });
 
 export default upload;
