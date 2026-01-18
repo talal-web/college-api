4431ec7c297732d0b311f6f428a423210d412890
talal-web <maliktalal910@gmail.com>
2026-01-18 17:45:44 +0500

    cookies setup

--- src/app.js
+++ src/app.js
@@ -12,15 +12,17 @@ import studentRoutes from "./routes/studentRoutes.js";
 import eventRoutes from "./routes/eventRoutes.js";
 import diaryRoutes from "./routes/diaryRoutes.js";
 import assignmentRoutes from "./routes/assignmentRoutes.js";
-import productRoutes from "./routes/productRoutes.js"
+import productRoutes from "./routes/productRoutes.js";
 
 const app = express();
 
 app.use(cookieParser());
-app.use(cors({
-  origin: "http://localhost:3000",
-  credentials: true  // important for cookies/sessions
-}));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true, // important for cookies/sessions
+  })
+);
 
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
@@ -28,22 +30,28 @@ app.use("/uploads", express.static("uploads"));
 // Optional: attach req.user before logging (only for authenticated routes)
 
 // Morgan request logging with user info
-app.use(morgan((tokens, req, res) => {
-  const userId = req.user?._id || "Guest";
-  const role = req.user?.role || "Guest";
+app.use(
+  morgan(
+    (tokens, req, res) => {
+      const userId = req.user?._id || "Guest";
+      const role = req.user?.role || "Guest";
 
-  return [
-    tokens.method(req, res),
-    tokens.url(req, res),
-    tokens.status(req, res),
-    tokens.res(req, res, 'content-length'),
-    "-",
-    tokens['response-time'](req, res), "ms",
-    `user:${role}:${userId}`
-  ].join(' ');
-}, {
-  stream: { write: (message) => logger.info(message.trim()) }
-}));
+      return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, "content-length"),
+        "-",
+        tokens["response-time"](req, res),
+        "ms",
+        `user:${role}:${userId}`,
+      ].join(" ");
+    },
+    {
+      stream: { write: (message) => logger.info(message.trim()) },
+    }
+  )
+);
 
 // Routes
 app.use("/api/auth", authRoutes);
@@ -51,7 +59,7 @@ app.use("/api/admin", protect, adminRoutes);
 app.use("/api/students", protect, studentRoutes);
 app.use("/api/events", eventRoutes);
 app.use("/api/diary", protect, diaryRoutes);
-app.use("/api/assignments", protect,  assignmentRoutes);
+app.use("/api/assignments", protect, assignmentRoutes);
 app.use("/api/product", protect, productRoutes);
 
 app.get("/", (req, res) => res.send("College Backend Running..."));
